b1b48e52aad74db5c7dc4d0d2e37d3249b8bddb7
Ìmíí <oluwatimely@gmail.com>
2025-10-01 12:23:28 +0100

    Completed step 6: implemented dict map, answer declarer that checks if user input is correct and the scoring system

--- test.py
+++ test.py
@@ -7,13 +7,20 @@ def selector():
 
 A_dict = selector()
 B_dict = selector()
+score = 0
+
 
 def selector_checker():
     global B_dict
     if A_dict == B_dict:
         B_dict = selector()
         return B_dict
-        
+ 
+dict_map = {
+    "A": A_dict,
+    "B": B_dict
+}
+
 selector_checker()
 #print(A_dict)
 #print(B_dict)
@@ -24,21 +31,48 @@ def commentator():
     print(art.logo)
     print(f"Compare A: {A_dict['name']}, {A_dict['description']}, {A_dict['country']}")
     print(art.vs)
-    user_input = input(f"With B: {B_dict['name']}, {B_dict['description']}, {B_dict['country']}:  ").lower()
+    user_input = input(f"With B: {B_dict['name']}, {B_dict['description']}, {B_dict['country']}:  ").upper()
 
 commentator()
-print(A_dict['follower_count'])
-print(B_dict['follower_count'])
+#print(A_dict['follower_count'])
+#print(B_dict['follower_count'])
 
 answer = ""
 def answer_checker():
     global answer
     if A_dict['follower_count'] > B_dict['follower_count']:
-        answer = A_dict['follower_count']
+        answer = A_dict
     elif A_dict['follower_count'] < B_dict['follower_count']:
-        answer = B_dict['follower_count']
-    print(answer)
+        answer = B_dict
     return answer
-    
+
 answer_checker()
-print(answer)
+
+def answer_declarer():
+    global score
+    if dict_map[user_input] == answer:
+        print(f"{answer['name']} is the correct answer.")
+        score += 1
+    else:
+        print("You have made an invalid choice")
+    return score
+    
+answer_declarer()
+print(f"Score is {score}")
+
+game_over = False
+while not game_over:
+   A_dict = selector()
+   B_dict = selector()
+   selector_checker()
+   dict_map = {
+    "A": A_dict,
+    "B": B_dict
+}
+   print(A_dict)
+   print(B_dict)
+   commentator()
+   answer_checker()
+   answer_declarer()
+   print(f"Score is {score}")
+   
